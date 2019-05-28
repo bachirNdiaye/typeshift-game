c0bdb6659eba2a1c93bbc39dd550298995eb9e9c
Mohameth El Bachir NDIAYE <ndiayeb44@gmail.com>
2019-05-28 16:53:06 +0000

    Changement de la valeur initiale de maxScore et création d'une liste chaînée qui représente la ligne du centre

--- globals.py
+++ globals.py
@@ -10,9 +10,12 @@ level = randint(3, 10)
 expectedWords = 3 if randint(0,1) else 5 #Le nombre prévu à trouver (correspond aussi au nombre de lignes), mais le joueur peut quand meme en trouver d'autres (Ils seront dans les mots additionels trouvés). Ici on choisi aléatoirement 3 ou 5 lignes et mots prévus
 allWordsOfThisLevel = []
 words = []
-maxScore = level*expectedWords #Le nombre de points total à avoir
+maxScore = 0 #On le modifiera plus tard
 additionalFoundWords = [] #Si le joueur trouve des mots en plus de ceux qui avaient étés prévus, ils seront ici
 columns = {} #Correspond aux colonnes du jeu
+centerLineList = { #Une liste chainée correspondant à la ligne du centre, un mot est trouvé si tous les noeuds de la liste forment un mot
+	"head": None
+}
 window = Tk()
 
 with open("config.json") as configFile:
