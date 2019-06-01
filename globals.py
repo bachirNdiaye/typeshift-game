7f9fc6b5293af1af22e2ccd5684a35da25aa57a2
Mohameth El Bachir NDIAYE <ndiayeb44@gmail.com>
2019-06-01 23:09:24 +0000

    Declaration de canvas dans globals , de foundWords, d'actualScore et de scoreWidgerText

--- globals.py
+++ globals.py
@@ -10,13 +10,16 @@ level = randint(3, 10)
 expectedWords = 3 if randint(0,1) else 5 #Le nombre prévu à trouver (correspond aussi au nombre de lignes), mais le joueur peut quand meme en trouver d'autres (Ils seront dans les mots additionels trouvés). Ici on choisi aléatoirement 3 ou 5 lignes et mots prévus
 allWordsOfThisLevel = []
 words = []
+actualScore = 0
 maxScore = 0 #On le modifiera plus tard
+foundWords = []
 additionalFoundWords = [] #Si le joueur trouve des mots en plus de ceux qui avaient étés prévus, ils seront ici
 columns = {} #Correspond aux colonnes du jeu
 centerLineList = { #Une liste chainée correspondant à la ligne du centre, un mot est trouvé si tous les noeuds de la liste forment un mot
 	"head": None
 }
 window = Tk()
+scoreWidgetText = StringVar()
 
 with open("config.json") as configFile:
 	config = json.load(configFile)
@@ -31,6 +34,9 @@ with open("config.json") as configFile:
 	foundLetterBlockColor				= config["foundLetterBlockColor"]
 	onCenterLineFoundLetterBlockColor	= config["onCenterLineFoundLetterBlockColor"]
 	onCenterLineLetterBlockColor		= config["onCenterLineLetterBlockColor"]
+	maxLetters 							= config["maxLetters"] #Le nombre de lettres maximum par mot du dictionnaire (A changer dans le fichier config.json)
+
+canvas = Canvas(window, width = gridContainerSize, height = gridContainerSize)
 
 centerLinePosX = 0
 centerLinePosY = (gridContainerSize // 2 - letterBlockSize // 2)
